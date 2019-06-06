2254669279854d401168f7139ebaa2cee989ccd5
Nathan Birch <birchn@byui.edu>
2019-06-06 07:16:41 -0600

    unknown changes

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="author" content="Nathan Birch">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="primary.css">
 		<title>Birch Home</title>
 	</head>
@@ -10,7 +11,34 @@
 		<div class="page-container">
 			<?php include 'header.php';?>
 			<div class="content-wrap">
-				<p>This is the body of index.php.</p>
+				<div class="container">
+				  <div class="row">
+				    <div class="col">
+				      1 of 3
+				    </div>
+				    <div class="col-6">
+				      2 of 3 (wider)
+				    </div>
+				    <div class="col">
+				      3 of 3
+				    </div>
+				  </div>
+				  <div class="row">
+				    <div class="col">
+				      1 of 3
+				    </div>
+				    <div class="col-5">
+				      2 of 3 (wider)
+				    </div>
+				    <div class="col">
+				      3 of 3
+				    </div>
+				  </div>
+				</div>
+
+
+
+
 			</div>
 			<?php include 'footer.php';?>
 		</div>
