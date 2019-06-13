31591ecf36ba8b2c3c47bd499b6b8570ebed2227
Nathan Birch <birchn@byui.edu>
2019-06-13 08:02:12 -0600

    working on videoLibray. Need to use Angular for reusability

---
+++ web/videoLibrary/cit160/cit160.php
@@ -0,0 +1,175 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Nathan Birch">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script> -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		
+		<script src="../primary.js"></script>
+		<link rel="stylesheet" href="/cs313/web/videoLibrary/home/primary.css">
+		<!-- <link rel="stylesheet" href="../movieLibrary.css"></link> -->
+		<script src="../movieLibrary.js"></script>
+		<title>CIT 160 Videos</title>
+	</head>
+	<body>
+		<?php include '../home/header.php';?>
+
+		<div>
+			
+			<div id="wrapper">
+  
+			  <span id="controlL" class="left-controls" role="button" aria-label="See Previous Modules">
+			    <b class="fa fa-chevron-left fa-chevron-left-extra" aria-hidden="true"></b>
+			  </span>
+			  
+			  <div class="title text-center h1">Module Selection Area</div>
+			 
+			  <div class="module-section clearfix">
+			    <!-- <button class="btn arrow-guides fa-chevron-left"></button> -->
+			   <ul id="content">
+			    
+			    <li class="card effect1">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			    <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			    <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			    <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			    <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			    </li>
+			     <li class="card">
+			      <div class="inside-top">
+			        <img src="http://placehold.it/350x150">
+			      </div>
+			       
+			    </li>
+			    
+			   </ul>
+			    
+			   
+			    
+			  </div><!--end of module-section-->
+			  
+			<span id="controlR" class="right-controls" role="button" aria-label="See Previous Modules">
+			    <b class="fa fa-chevron-right fa-chevron-right-extra" aria-hidden="true"></b>
+			  </span>
+			<!-- <button class="btn arrow-guides-right fa-chevron-right"></button> -->
+			  
+			  
+			  
+			</div>
+
+
+
+
+
+		</div>
+		<?php include '../home/footer.php';?>
+
+	</body>
+</html>
--- web/videoLibrary/cs313/cs313.php
+++ web/videoLibrary/cs313/cs313.php
@@ -4,18 +4,17 @@
 		<meta charset="utf-8">
 		<meta name="author" content="Nathan Birch">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="/cs313/web/videoLibrary/home/primary.css">
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		<script src="primary.js"></script>
-		<title>Birch Home</title>
+		<title>CS 313</title>
 	</head>
 	<body>
-		<?php include 'home/header.php';?>
+		<?php include '../home/header.php';?>
+		
 		<div class="page-container">
-			HOME PAGE TESTING
+			CS 313 TESTING
 		</div>
-		<?php include 'home/footer.php';?>
+		<?php include '../home/footer.php';?>
 	</body>
 </html>
