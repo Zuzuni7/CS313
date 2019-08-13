4d307b39046fbdb8b4be115bb38e05c1f6df16bc
Nathan Birch <birchn@byui.edu>
2019-08-13 15:40:53 -0600

    not really sure what I'm committing.... :(

--- web/assignments/assignments.php
+++ web/assignments/assignments.php
@@ -1,26 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="en-us">
-	<head>
-		<meta charset="utf-8">
-		<meta name="author" content="Nathan Birch">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="assignments.css">
-		<title>Birch Home</title>
-	</head>
-	<body>
-		<?php include '../shared/header.php';?>
-		<br><br>
-		<div class="page-container">
-			<h1>Select Week</h1>
-			<div id="list2">
-			   <ol>
-			      <a href="../week1/hello"><li><p><em>Hello World Program</em></p></li></a>
-			      <a href="../week2"><li><p><em>Team Exercise: jquery, bootstrap, changing colors programatically.</em>Assignment: Personal Introduction</p></li></a>
-			      <li><p><a href="../week3"><em>Team Exercise: PHP Form</em></a><a  href="../week3/shopping.php">Shopping Cart</a></p></li>
-			      <li><p><a href="../week4/sql.php"><em>SQL Team Activty</em></a>Personal DB Creation is found in the db folder.</p></li></a>
-			      <li><p><a href="../week5"><em>Team Activity: SQL Scriptures</em></a>Personal DB Creation is found in the db folder.</p></li></a>
-			   </ol>
-			</div>
-		<?php include '../shared/footer.php';?>
-	</body>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Nathan Birch">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assignments.css">
+	<title>Birch Home</title>
+</head>
+
+<body>
+	<?php include '../shared/header.php'; ?>
+	<br><br>
+	<div class="page-container">
+		<h1>Assignments</h1>
+		<div id="list2">
+			<ol>
+				<a href="../week1/hello">
+					<li>
+						<p><em>Hello World Program</em></p>
+					</li>
+				</a>
+				<a href="../week2">
+					<li>
+						<p><em>Team Exercise: jquery, bootstrap, changing colors programatically.</em>Assignment: Personal Introduction</p>
+					</li>
+				</a>
+				<li>
+					<p><a href="../week3"><em>Team Exercise: PHP Form</em></a><a href="../week3/shopping.php">Shopping Cart</a></p>
+				</li>
+				<li>
+					<p><a href="../week4/sql.php"><em>SQL Team Activty</em></a>Personal DB Creation is found in the db folder.</p>
+				</li></a>
+				<li>
+					<p><a href="../week5"><em>Team Activity: SQL Scriptures</em></a>Personal DB Creation is found in the db folder.</p>
+				</li></a>
+			</ol>
+		</div>
+		<?php include '../shared/footer.php'; ?>
+</body>
+
 </html>
