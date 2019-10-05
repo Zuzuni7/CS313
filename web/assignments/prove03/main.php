6fa3a4b68482989973649cc1f0a2baf4bd13a94b
Your Name <you@example.com>
2019-10-05 12:00:23 -0600

    Pushed main.php

---
+++ web/assignments/prove03/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Bamazon</title>
+</head>
+<body>
+    <div>
+        <h1>Bamazon</h1>
+        <br/>
+        <div class="items">
+            <!--displays items in a list. Onclick will give you the option
+                 to add to cart.-->
+            <a href="cart.html">View Your Cart</a>
+        </div>
+    </div>
+</body>
+</html>
