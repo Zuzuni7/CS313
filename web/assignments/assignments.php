d5bb378d3b84e6e2093f0e0b4be0c3ee2e9114e5
Your Name <you@example.com>
2019-10-05 23:12:51 -0600

    updated assign 01

--- web/assignments/assignments.php
+++ web/assignments/assignments.php
@@ -4,7 +4,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="author" content="Shalom Sims">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="assignments.css">
 	<title>Shalom in the Home</title>
 </head>
@@ -16,7 +15,7 @@
 		<h1>Assignments</h1>
 		<div id="list2">
 			<ol>
-				<a href="../week1/hello">
+				<a href="../week1/hello.html">
 					<li>
 						<p><em>Hello World Program</em></p>
 					</li>
