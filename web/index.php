ddb263189e622c840c005370774e7586623cc681
Your Name <you@example.com>
2019-10-05 23:22:40 -0600

    updated

--- web/index.php
+++ web/index.php
@@ -3,9 +3,6 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="primary.css">
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<title>Shalom Home</title>
 	</head>
 	<body>
@@ -25,24 +22,9 @@
 				    </div>
 				    <div class="col">
 						<a href="assignments/assignments.php">
-							<div class="card" >
-								<!-- <img class="card-img-top" src="files/code.jpg" alt="Card image cap"> -->
-								<div class="card-body">
-							    	<p class="card-text">Assignments</p>
-								</div>		
-							</div>
+							<p class="card-text">Assignments</p>
 						</a>
 				    </div>
-				    <!-- <div class="col">
-						<a href="videoLibrary/index.php">
-							<div class="card" >
-								 <img class="card-img-top" src="files/library.jpeg" alt="Card image cap">
-								<div class="card-body">
-							    	<p class="card-text">Project: Video Library</p>
-								</div>		
-							</div>
-						</a>
-				    </div> -->
 				  </div>
 				</div>
 			</div>
