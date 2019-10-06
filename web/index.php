1cbbd30b0e419bfd7eba1a7df1fbb5f04fe2418c
Your Name <you@example.com>
2019-10-05 23:31:24 -0600

    index.php update

--- web/index.php
+++ web/index.php
@@ -8,26 +8,9 @@
 	<body>
 		<?php include 'shared/header.php';?>
 		<div class="page-container">
-			<div class="content-wrap top-space">
-				<div class="container">
-				  <div class="row">
-				    <div class="col">
-						<a href="assignments/prove02/homepage.html">
-							<div class="card" >
-								<div class="card-body">
-							    	<p class="card-text">About Me</p>
-								</div>		
-							</div>
-						</a>
-				    </div>
-				    <div class="col">
-						<a href="assignments/assignments.php">
-							<p class="card-text">Assignments</p>
-						</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
+			<a href="assignments/assignments.php">
+			<br/>
+			<a href="assignments/prove02/homepage.html">
 			<?php include 'shared/footer.php';?>
 		</div>
 	</body>
