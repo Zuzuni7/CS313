31a0429ee8eda2559db2de97f84a3bedaf92a202
Your Name <you@example.com>
2019-10-05 19:37:56 -0600

    changed remote

--- web/assignments/prove03/main.php
+++ web/assignments/prove03/main.php
@@ -1,20 +1,118 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible">
-    <title>Bamazon</title>
+    <link rel="stylesheet" href="assign03stylesheet.css" />
 </head>
-<body>
-    <div>
-        <h1>Bamazon</h1>
-        <br/>
-        <div class="items">
-            <!--displays items in a list. Onclick will give you the option
-                 to add to cart.-->
-            <a href="cart.html">View Your Cart</a>
+<style>
+    p.footer {
+        background-color: black;
+        color: white;
+        font-family: Century;
+    }
+</style>
+    <title> Roadie's </title>
+
+    <header class="relative" style="font: bold 34px century;">
+        <div class="relative" style="color: black; text-decoration: dashed">
+            Roadie's Inc.
         </div>
-    </div>
-</body>
+        <div class="sticky">
+            <img src="assign03/lebike.png" alt="Bike.jpg"; style="max-width: 200px; max-height: 100%" />
+            <img src="assign03/lebike.png" alt="Bike.jpg"; style="max-width: 200px; max-height: 100%" />
+        </div>
+
+    </header>
+
+    <body background="assign03/bg2.jpg" class="center">
+        
+        <div class="relative" ; text-align: left">
+            <a href="bikelisting.html" style="font: bold 20px century ">  See Our Inventory </a>
+        </div>
+        <div class="relative" ; text-align: left">
+            <a href="assign03/assign03forms.html" style="font: bold 20px century ">  Become a Member </a>
+        </div>
+        <div class="relative" ; text-align: left">
+            <a href="#bottom" ; style="font: bold 20px century "> Contact Us </a>
+        </div>
+
+        <div class="relative" ;>
+
+            <p style="color: black; font: 15px century">
+                Welcome to Roadie's! The home of your cycling needs! We are a private
+
+                company that specializes in catering to  those brave souls who traverse
+                these car-dominated roads with their two-wheeled chariots!
+                </br>
+                </br>
+                <em style="font: bold 18px; text-decoration-line: underline"> About us </em>
+                </br>
+                Founded in 2018 by Shalom Sims, Rodie's Inc. is
+                a small company based out of local Rexburg, ID. The company was founded to
+                cater to the needs of the many biking students on the campus of BYU-Idaho
+                and the sightseers who explore the back-roads of Idaho. The company specializes
+                in older roadbike designs but is also open to newer high speed bike builds. 
+                </br>
+                </br>
+                <div style="text-align: center">
+                    <em style="font: bold 15px century; text-align: center">
+                        "Be who you are and say what you feel, because those who mind don't matter, 
+                        and those who matter don't mind."
+                    </em>
+                    <em style="font: intalic 15px century; text-align: center">  -Bernard M. Maruch </em>
+                </div>
+
+            </p>
+
+        </div>
+        </br>
+        </br>
+
+        <div class="img.a">
+            <a href="bikelisting.html"> Classic Bike Parts</a>
+            <img class="left" src="assign03/bikedeco2.jpg" />
+
+        </div>
+
+        <div class="img.a">
+            <img src="assign03/bikedeco4.jpg" />
+        </div>
+
+        <div class="relative" style="background-color: black">
+            <p style="color: white; font: 15px century">
+                LOCATION
+                </br>
+                Our office is located
+                at:
+                </br>
+                431 S 3rd
+                </br>
+                Unit #1108
+                </br>
+                Rexburg, ID 83440
+            </p>
+            <a name="bottom"> </a>
+            <p class="footer">
+                Office number: (208)999-9999
+                </br>
+                Office Hours: Monday-Friday 9am-7pm, Saturday 8am-4pm
+                </br>
+                Email: sim16024@byui.edu
+                </br>
+
+
+            </p>
+        </div>
+
+    </body>
+    <footer>
+        <div class="relative">
+            <a href="index.html"; style="text-align: center; font: bold 20px castellar"> Back to assignments </a>
+        </div>
+    </footer>
 </html>
+
+<!clip images of bike tire>
+<!>
+<!>
+<!>
+<!>
