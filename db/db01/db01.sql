bb1d93aadf71c3eb3800b8797b368d7914a7c7ac
Your Name <you@example.com>
2019-10-09 14:17:30 -0600

    brother birch hacking

---
+++ db/db01/db01.sql
@@ -0,0 +1,63 @@
+CREATE TABLE users 
+( user_id           INTEGER         PRIMARY KEY
+, username          VARCHAR(30)     NOT NULL   
+);
+
+CREATE SEQUENCE users_n1 START WITH 1001;
+
+CREATE TABLE speakers 
+( speaker_id        INTEGER         PRIMARY KEY
+, first_name        VARCHAR(50)     NOT NULL
+, last_name         VARCHAR(50)     NOT NULL
+, calling           VARCHAR(256)    NOT NULL
+);
+
+CREATE SEQUENCE speakers_n1 START WITH 1001;
+
+CREATE TABLE conferences
+( conference_id     INTEGER         PRIMARY KEY 
+, speaker_id        INTEGER         REFERENCES speakers(speaker_id)
+, conference_date   DATE            NOT NULL
+);
+
+CREATE SEQUENCE conferences_n1 START WITH 1001;
+
+CREATE TABLE notes
+( note_id           INTEGER         PRIMARY KEY
+, user_id           INTEGER         REFERENCES users(user_id)
+, talk_id           INTEGER         REFERENCES talks(talk_id)
+, note_content      VARCHAR(256)
+);
+
+CREATE SEQUENCE notes_n1 START WITH 1001;
+
+CREATE TABLE talks 
+( talk_id           INTEGER         PRIMARY KEY
+, conference_id     INTEGER         REFERENCES conferences(conference_id)
+, speaker_id        INTEGER         REFERENCES speakers(speaker_id)
+);
+
+CREATE SEQUENCE talk_n1 START WITH 1001;
+
+INSERT INTO conferences
+( conference_id
+, speaker_id
+, conference_date
+) 
+VALUES 
+( conferences_n1.NEXTVAL
+, 
+, 
+);
+INSERT INTO talks (talk_id, conference_id, speaker_id) VALUES ();
+
+INSERT INTO notes 
+( note_id
+, user_id
+, talk_id
+, note_content
+)
+VALUES
+{ notes_n1.NEXTVAL
+, 
+}
--- web/assignments/prove03/cart.php
+++ web/assignments/prove03/cart.php
@@ -7,8 +7,18 @@
     <title>Your Cart</title>
 </head>
 <body>
-    <?php
-    echo "Your cart."
-    ?>
+
+    Items currently in your cart: <?php echo $_GET["numTires"]; ?><br>
+    <?php echo $_GET["tires"] ?>
+    <br>
+
+    Items currently in your cart: <?php echo $_GET["numTubes"]; ?><br>
+    <?php echo $_GET["tubes"] ?>
+    <br>
+
+    Items currently in your cart: <?php echo $_GET["numBrakes"]; ?><br>
+    <?php echo $_GET["brakes"] ?>
+    <br>
+
 </body>
 </html>
