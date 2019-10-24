1a0207f361a6b989a3dc5abe8ae9304824907668
Your Name <you@example.com>
2019-10-24 13:08:23 -0600

    Bootstrapping and stuff

--- web/assignments/assignments.php
+++ web/assignments/assignments.php
@@ -8,7 +8,7 @@ $page = "home";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="assignments.css">
+    <link rel="stylesheet" href="primary.css">
     <title>Shalom's Homepage</title>
     <style>
         body {
