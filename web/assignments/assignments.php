d9fb219d11190f970077b7d34af76926b7e14f1b
Your Name <you@example.com>
2019-10-24 12:25:21 -0600

    updating webpage

--- web/assignments/assignments.php
+++ web/assignments/assignments.php
@@ -7,7 +7,8 @@ $page = "home";
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" >
     <title>Shalom's Homepage</title>
     <style>
         body {
