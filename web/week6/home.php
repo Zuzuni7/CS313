e40697f1ad6a95720d8892018cf82bdbbbdb600f
Your Name <you@example.com>
2019-10-25 19:08:15 -0600

    index

--- web/week6/home.php
+++ web/week6/home.php
@@ -21,18 +21,27 @@ require 'dbConnect.php';
     </div>
     <?php
         session_start();
-
+        
         if(!empty($_POST)) {
             if (isset($_POST['username']) && isset($_POST['password'])) {
                 $username = $_POST['username'];
                 $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username");
                 $stmt->bindValue(':username', $username);
                 $stmt->execute();
-                $result =$stmt->get_result();
+                $result = $stmt->get_result();
                 $user = $result->fetch_object();
+                
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+				{
+                    $user_id = $row['user_id'];
+                    $username = $row['username'];
+                    $password = $row['password'];
+
+                    echo "<p>$user_id "." $username "." $password</p>";
+				}
 
                 echo "<h1>$username and $user->password</h1>";
-                
+
                 if($_POST['password'] = user.password){
                     $_SESSION['user_id'] = $user->ID;
 
