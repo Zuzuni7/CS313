3deea7359bceffc7bfe45581f92e81718b4f59fa
Your Name <you@example.com>
2019-10-25 19:30:00 -0600

    prove06

--- web/week6/login.php
+++ web/week6/login.php
@@ -15,8 +15,8 @@
     <?php
     
 
-    // if(!empty($_POST)) {
-    //     if (isset($_POST['username']) && isset($_POST['password'])) {
+    if(!empty($_POST)) {
+         if (isset($_POST['username']) && isset($_POST['password'])) {
     //         $username = $_POST['username'];
     //         $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username");
     //         $stmt->bindValue(':username', $username);
@@ -36,13 +36,12 @@
     //             $_SESSION['user_id'] = $user->ID
     //             echo "<h1>$username and $user->password</h1>";
     //         }
-    //     }
-    //     else {
-    //         echo "<p>You must fill out the entire form!</p>";
-    //     }
-    // }else{
-    //     echo "<p>You must fill out the username!</p>";
-    // }
+         }else {
+            echo "<p>You must fill out the entire form!</p>";
+         }
+    }else{
+        echo "<p>You must fill out the username!</p>";
+    }
 ?>   
     </body>
     </html>
