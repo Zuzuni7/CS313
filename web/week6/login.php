5ff359ed8a102dc7c4925accad24fd8d2ede2199
Your Name <you@example.com>
2019-10-25 20:09:45 -0600

    prove06

--- web/week6/login.php
+++ web/week6/login.php
@@ -19,7 +19,7 @@
         if (isset($_POST['username']) && isset($_POST['password'])) {
             $username = $_POST['username'];
             $password = $_POST['password'];
-            $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username");
+            $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username AND password = :password");
             $stmt->bindValue(':username', $username);
             $stmt->bindValue(':password', $password);
             $stmt->execute();
