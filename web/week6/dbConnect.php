ff944cfde74efa503ae954bac3c880a1c29517d8
Your Name <you@example.com>
2019-10-25 19:28:22 -0600

    prove06

--- web/week6/dbConnect.php
+++ web/week6/dbConnect.php
@@ -1,30 +1,9 @@
-<?php
-try {
-    $dbUrl = getenv('DATABASE_URL');
-    $dbOpts = parse_url($dbUrl);
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"], '/');
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $_SESSION['db'] = $db;
-} catch (PDOException $ex) {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-}
-?>
-
 
 <?php
 function get_db() {
 	$db = NULL;
 	try {
 		$dbUrl = getenv('DATABASE_URL');
-		// if (!isset($dbUrl) || empty($dbUrl)) {
-		// 	$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
-		// }
 		$dbopts = parse_url($dbUrl);
 		$dbHost = $dbopts["host"];
 		$dbPort = $dbopts["port"];
@@ -40,3 +19,4 @@ function get_db() {
 	}
 	return $db;
 }
+?>
