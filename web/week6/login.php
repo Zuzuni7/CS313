e3d84d3d42d66ee543f290cd470b54aacb3ed995
Your Name <you@example.com>
2019-10-25 19:33:52 -0600

    prove06

--- web/week6/login.php
+++ web/week6/login.php
@@ -16,7 +16,7 @@
     
 
     if(!empty($_POST)) {
-         if (isset($_POST['username']) && isset($_POST['password'])) {
+        if (isset($_POST['username']) && isset($_POST['password'])) {
             $username = $_POST['username'];
             $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username");
             $stmt->bindValue(':username', $username);
@@ -24,22 +24,27 @@
             $result = $stmt->get_result();
             $user = $result->fetch_object();
 
-    //         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-    // 		{
-    //             $user_id = $row['user_id'];
-    //             $username = $row['username'];
-    //             $password = $row['password']
-    //             echo "<p>$user_id "." $username "." $password</p>";
-            
-    //         echo "<h1>$username and $user->password</h1>"
-    //         if($_POST['password'] = user.password){
-    //             $_SESSION['user_id'] = $user->ID
-    //             echo "<h1>$username and $user->password</h1>";
-    //         }
-         }else {
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+     		{
+                 $user_id = $row['user_id'];
+                 $username = $row['username'];
+                 $password = $row['password']
+                 echo "<p>$user_id "." $username "." $password</p>";
+      
+                echo "<h1>$username and $user->password</h1>"
+                if($_POST['password'] = user.password){
+                    $_SESSION['user_id'] = $user->ID
+                    echo "<h1>$username and $user->password</h1>";
+                }
+            }
+        }
+        else 
+        {
             echo "<p>You must fill out the entire form!</p>";
-         }
-    }else{
+        }
+    }
+    else
+    {
         echo "<p>You must fill out the username!</p>";
     }
 ?>   
