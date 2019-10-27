d1318d76fabd571a5660088d0473dcd9c9fee44d
Your Name <you@example.com>
2019-10-26 22:38:17 -0600

    Profile.php

--- web/week6/home.php
+++ web/week6/home.php
@@ -4,6 +4,7 @@ require 'dbConnect.php';
 if (isset($_SESSION['login_user'])){
     header("location: profile.php");
 }
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,8 +18,46 @@ if (isset($_SESSION['login_user'])){
 </head>
 <body>
     <h1>Reminisce: Homepage</h1>
+
     <div class="page-container">
-        <form action="login.php" method="post">
+        <?php
+            if (isset($_POST['username']) && isset($_POST['password'])) 
+            {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $stmt = $db->prepare("SELECT * FROM user_ WHERE username = :username AND user_password = :passwrd");
+                $stmt->bindValue(':username', $username);
+                $stmt->bindValue(':passwrd', $password);
+                $stmt->execute();
+    
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+                 {
+                    $user_id = $row['user_id'];
+                    $username = $row['username'];
+                    $pswd = $row['user_password'];
+                    echo "<p>$user_id "." $username "." $pswd</p>";
+    
+                    if($pswd == $password)
+                    {
+                        $_SESSION['user_id'] = $username;
+                        header("location: profile.php");
+                        $db = get_db();
+                        //echo "<h1>$username and $password</h1>";
+                    }
+                    else
+                    {
+                        $e = "Incorrect Password.";
+                    }
+                }
+            }
+            else 
+            {
+                $e = "You must fill out the entire form.";
+            }
+        ?>
+        </div>
+        <div class="page-container">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <div class="form-group row">
                 <div class="col-sm-10">
                     <input type="text" name="username" placeholder="Enter your username" required>
