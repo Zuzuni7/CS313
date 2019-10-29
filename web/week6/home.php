5c890a6f6e424e3bf53c153c804087b3d24e1f4e
Your Name <you@example.com>
2019-10-29 17:13:58 -0600

    website updates

--- web/week6/home.php
+++ web/week6/home.php
@@ -13,8 +13,8 @@ if (isset($_SESSION['user_id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css\fancy.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge,chrome=1">
+    <link rel="stylesheet" type="text/css" href="css/fancy.css">
     <link rel="stylesheet" type="text/css" href="../bootstrap-4.3.1-dist/css/bootstrap.css">
     <!-- <link rel="stylesheet" href="primary.css"> -->
 	<title>Reminisce</title>
