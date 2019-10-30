8e94f8eb0567934868a1a39e3be73a255c427626
Your Name <you@example.com>
2019-10-29 19:43:34 -0600

    website updates

--- web/week6/profile.php
+++ web/week6/profile.php
@@ -11,7 +11,8 @@ $_SESSION["user_id"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="../css/fancy.css" rel="stylesheet" type="text/css" >
-    
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+
     <title>Profile Page</title>
 </head>
 <body>
