6262d7514ff4842be918a28ad70f1fd4d21c4df9
Shalom Sims <sim16024@byui.edu>
2019-11-01 15:44:53 -0600

    website updates

--- web/week6/login.php
+++ web/week6/login.php
@@ -8,12 +8,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
         <link href="../css/fancy.css" rel="stylesheet" type="text/css" >
     
         <title>Login Page</title>
     </head>
     <body>
     <h1>Welcome<h1>
+    <div class="container">
+        <div class="login">
+            <form method="post" action="login.php">
+                <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
+                <p><input type="password" name="password" value="" placeholder="Password"></p>
+                <p class="remember_me">
+                    <label>
+                        <input type="checkbox" name="remember_me" id="remember_me">
+                        Remember me on this computer
+                    </label>
+                </p>
+                <p class="submit"><input type="submit" name="commit" value="Login"></p>
+            </form>
+        </div>
+        <div class="login-help">
+            <p>Forgot your password? <a href="#">Click here to reset it</a>.</p>
+        </div>
+    
+    </div>
     <?php
     //session_start();
     $e = '';
