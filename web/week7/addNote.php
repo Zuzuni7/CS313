b78e4831b5c96e6e39031930f640dad2768e9e45
Shalom Sims <sim16024@byui.edu>
2019-11-02 15:43:11 -0600

    website updates

--- web/week7/addNote.php
+++ web/week7/addNote.php
@@ -9,6 +9,7 @@ include 'dbConnect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link href="../css/fancy.css" rel="stylesheet" type="text/css" >
     <title>Notes</title>
 </head>
 <body>
