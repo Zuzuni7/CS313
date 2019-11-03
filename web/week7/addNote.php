a26fb9453c40ebf434ff0e408248069857162464
Shalom Sims <sim16024@byui.edu>
2019-11-03 03:04:56 -0700

    inserts

--- web/week7/addNote.php
+++ web/week7/addNote.php
@@ -29,13 +29,13 @@ try {
     //echo "<p>about to make the insert query.</p>"; // debugging
     $query = 'INSERT INTO daily_entry(user_id, entry_type, entry_text, title, created_date)VALUES(:_user_id, :_status, :_entry, :_title,:_date)';
     $stmt = $db->prepare($query);
-    //echo "post insert query";
+    echo "post insert query";
     $stmt->bindValue(':_user_id',$user_id);
     $stmt->bindValue(':_entry',$entry);
     $stmt->bindValue(':_title',$title);
     $stmt->bindValue(':_status',$status);
     $stmt->bindValue(':_date', $date);
-    //echo "dd it execute yet? nah fam.";
+    echo "dd it execute yet? nah fam.";
     $stmt->execute();
     echo "Executed the query";
     if ($db->query($sql) == TRUE) {
